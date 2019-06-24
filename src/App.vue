a1921327da53616bb3ddd9b13becf2d009562b80
jimpei <j.hamamura0621@gmail.com>
2019-06-24 23:46:11 +0900

    comment

--- src/App.vue
+++ src/App.vue
@@ -48,32 +48,37 @@ export default {
       // Initialize Firebase
       firebase.initializeApp(firebaseConfig);
 
-      var provider = new firebase.auth.GithubAuthProvider();
-      return firebase
-        .auth()
-        .signInWithPopup(provider)
-        .then(function(result) {
-          // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-          var token = result.credential.accessToken;
-          // The signed-in user info.
-          var user = result.user;
-          console.log('debug ' + user.email);
-          return user.email;
-          // ...
-        })
-        .catch(function(error) {
-          console.log('error');
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          console.log(error);
-          return 'error';
-          // ...
-        });
+      // ここで認証する
+      // var provider = new firebase.auth.GithubAuthProvider();
+      // return firebase
+      //   .auth()
+      //   .signInWithPopup(provider)
+      //   .then(function(result) {
+      //     // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+      //     var token = result.credential.accessToken;
+      //     // The signed-in user info.
+      //     var user = result.user;
+      //     console.log('debug ' + user.email);
+      //     return user.email;
+      //     // ...
+      //   })
+      //   .catch(function(error) {
+      //     console.log('error');
+      //     // Handle Errors here.
+      //     var errorCode = error.code;
+      //     var errorMessage = error.message;
+      //     // The email of the user's account used.
+      //     var email = error.email;
+      //     // The firebase.auth.AuthCredential type that was used.
+      //     var credential = error.credential;
+      //     console.log(error);
+      //     return 'error';
+      //     // ...
+      //   });
+
+
+
+
       // var name, email, photoUrl, uid, emailVerified, token;
       // firebase
       //   .auth()
